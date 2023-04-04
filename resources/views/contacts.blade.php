d84b3bc6afdcb2792bc452c985dfa6252f665b5d
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-04-04 17:53:25 +0200

    Feat create route e views

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel-primi-passi</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Contatti</h1>
+        <ul>
+        <li>Contatto 1</li>
+        <li>Contatto 2</li>
+        <li>Contatto 3</li>
+        <li>Contatto 4</li>
+        <li>Contatto 5</li>
+        <li>Contatto 6</li>
+        </ul>
+    </div>
+</body>
+</html>
