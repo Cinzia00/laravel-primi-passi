160be570ed69735a1e32e372d90a58422583adf2
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-04-04 16:36:22 +0200

    Feat creata views home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel-primi-passi</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Hello World</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam debitis voluptatem, aliquid totam voluptates impedit officiis consequatur ut ea enim tenetur eveniet voluptatum maiores ipsam. Ipsum vitae repellat cum. Est similique corrupti officiis sint facere voluptatibus inventore libero aliquam, quis itaque? Impedit aspernatur rerum porro quisquam id qui dolorem ad suscipit tempora a temporibus, voluptas perferendis, ducimus veritatis quis eum alias? Dolore ullam odit voluptates cum quidem harum, a assumenda, natus aliquid suscipit, odio inventore fuga consequuntur quo porro corporis deserunt voluptatem nisi perferendis. Vero sapiente repudiandae adipisci, at ipsum blanditiis ratione enim aperiam tempore ullam nihil quasi modi sint hic iure dolore. Ipsa culpa earum facere quos omnis reprehenderit, blanditiis ipsum atque suscipit laboriosam vero quidem nam dolor perspiciatis natus cupiditate animi nisi adipisci recusandae quaerat tempore maxime necessitatibus repellat debitis? Inventore voluptatum, incidunt rerum pariatur rem iste adipisci possimus labore laboriosam beatae tenetur sint sunt ratione, autem sapiente, aliquid sit quidem cupiditate dolores nulla officia facere! Harum, quasi officiis. Dolor assumenda dolorum doloribus magni? Ab dolores quos veniam obcaecati voluptatem accusantium minima voluptate, sit dolorem maiores similique inventore, fugiat ratione beatae possimus magni maxime facilis debitis voluptatibus reiciendis! Sunt, nam at voluptatem provident magni quia. Qui, blanditiis nam?</p>
+    </div>
+</body>
+</html>
